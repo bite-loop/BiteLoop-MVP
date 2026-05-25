da1a904e061942c90532d4032766405d6f4b66a4
IgYaHiko <subhrokolay2@gmail.com>
2026-05-26 01:12:14 +0530

    [POST]-[13]:google-login-setup-entire-app

--- app/(feed)/feed/page.tsx
+++ app/(feed)/feed/page.tsx
@@ -1,8 +1,32 @@
+'use client'
+import { Button } from '@/components/ui/button'
+import { useAuthStore } from '@/lib/stores/authStore'
+import { useRouter } from 'next/navigation'
+import { AnyAaaaRecord } from 'node:dns'
 import React from 'react'
+import { toast } from 'sonner'
 
 const Feed = () => {
+    const route = useRouter()
+    const {logout} = useAuthStore()
+    const handlelogout = async () => {
+        try {
+          await logout()
+          route.push("/login")
+          toast.success("logout successfully")
+
+        } catch (error: any) {
+          toast.error("logout failed")
+        }
+    }
   return (
-    <div>Feed</div>
+    <div className='py-24 px-24'>
+        <Button
+        onClick={handlelogout}
+        variant={'destructive'}>
+            Logout
+        </Button>
+    </div>
   )
 }
 
